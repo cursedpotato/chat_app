2b3de5615271196dfc249193c100331f7c344d85
Daniel <danireysan@gmail.com>
2022-02-16 12:23:33 -0600

    Adding functions to locally save user data

--- lib/helperfunctions/sharedpreferences_helper.dart
+++ lib/helperfunctions/sharedpreferences_helper.dart
@@ -1,3 +1,18 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
 class SharedPreferencesHelper {
 
+  static String userIdKey = "USERKEY";
+  static String userNameKey = "USERNAMEKEY";
+  static String displayNameKey = "USERDISPLAYNAMEKEY";
+  static String userEmailKey =  "USEREMAILKEY";
+  static String userProfileKey = "USERPROFILEKEY";
+
+  // NOTE: I think the following code can be optimized to a single function with a switch case
+  // Saving data with sharedPreferences
+  saveUserName(String getUserName) async{
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    return preferences.setString(userIdKey, getUserName);
+  }
+   
 }
