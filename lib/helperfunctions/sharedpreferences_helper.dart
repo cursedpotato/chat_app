13e077d2cafc03ac65994eb91295c67d5e79f030
Daniel <danireysan@gmail.com>
2022-02-16 12:41:18 -0600

    Added all the functions to get user data

--- lib/helperfunctions/sharedpreferences_helper.dart
+++ lib/helperfunctions/sharedpreferences_helper.dart
@@ -10,9 +10,30 @@ class SharedPreferencesHelper {
 
   // NOTE: I think the following code can be optimized to a single function with a switch case
   // Saving data with sharedPreferences
-  saveUserName(String getUserName) async{
+  Future<bool> saveUserId(String getUserId) async{
     SharedPreferences preferences = await SharedPreferences.getInstance();
-    return preferences.setString(userIdKey, getUserName);
+    return preferences.setString(userIdKey, getUserId);
   }
+  
+  Future<bool> saveUserName(String getUserName) async{
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+      return preferences.setString(userNameKey, getUserName);
+  }
+
+  Future<bool> saveDisplayName(String getDisplayName) async{
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    return preferences.setString(displayNameKey, getDisplayName);
+  }
+
+  Future<bool> saveUserEmail(String getUserEmail) async{
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    return preferences.setString(userEmailKey, getUserEmail);
+  }
+
+  Future<bool> saveUserProfile(String getUserProfile) async{
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    return preferences.setString(userProfileKey, getUserProfile);
+  }
+
    
 }
