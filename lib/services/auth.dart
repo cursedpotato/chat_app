6da93853ae84180003b27a4c95066f2f7d0da50e
Daniel <danireysan@gmail.com>
2022-02-16 12:00:42 -0600

    Created class for sharedpreferences and updated the minsdk to 21

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,3 +1,5 @@
+
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:google_sign_in/google_sign_in.dart';
@@ -16,8 +18,22 @@ class AuthMethods {
     //NOTE: Seems that this may need a try and and catch in the future
     // Accesing to a method of the class which returns a future 
     final GoogleSignInAccount? _googleSignInAccount = await _googleSingIn.signIn();
-    //NOTE: It may throw null and the the type may have to refactored as such: GoogleSingInAuthentication?  
+    //NOTE: It may throw null and the the type may have to refactored as such: GoogleSingInAuthentication?
+    // Calling the api to give auth params  
     final GoogleSignInAuthentication googleSignInAuthentication = await _googleSignInAccount!.authentication;
-    final AuthCredential credential = GoogleAuthProvider.credential(accessToken: googleSignInAuthentication.idToken, idToken: googleSignInAuthentication.accessToken, );
+    // Using those params to create a credential
+    final AuthCredential credential = GoogleAuthProvider.credential(
+      accessToken: googleSignInAuthentication.idToken, 
+      idToken: googleSignInAuthentication.accessToken
+    );
+    // Using that credential to sign in
+    final UserCredential? result = await _firebaseAuth.signInWithCredential(credential);
+
+    User? userDetails = result?.user;
+
+    if (result == null) {
+      debugPrint('This thing is null $result');     
+    } else {
+    }
   }
 }
