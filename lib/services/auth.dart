cb5f19044c6ccbda91a5a7c582b06ead157f649e
Daniel <danireysan@gmail.com>
2022-02-17 20:22:31 -0600

    commit on master

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,23 +1,19 @@
-
 import 'package:chat_app/helperfunctions/sharedpreferences_helper.dart';
-import 'package:chat_app/views/home.dart';
+import 'package:chat_app/services/database.dart';
+import 'package:chat_app/views/chat_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
-import 'database.dart';
-
-class AuthMethods extends ChangeNotifier{
+class AuthMethods {
   final FirebaseAuth auth = FirebaseAuth.instance;
 
-  getCurrentUser() {
-    return auth.currentUser;
+  getCurrentUser()async {
+    // ignore: await_only_futures
+    return await auth.currentUser;
   }
 
-<<<<<<< HEAD
-  final googleSignIn = GoogleSignIn();
-=======
   signInWithGoogle(BuildContext context) async {
     final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
     // Creating a var for easily accesing the instance of a class
@@ -37,49 +33,26 @@ class AuthMethods extends ChangeNotifier{
     // Using that credential to sign in
     final UserCredential? result =
         await _firebaseAuth.signInWithCredential(credential);
->>>>>>> tmp
 
-  GoogleSignInAccount? _user;
-
-  GoogleSignInAccount get user => _user!;
-
-  Future signInWithGoogle(BuildContext context) async {
-    // Accesing to a method of the class which returns a future
-    final googleUser =  await googleSignIn.signIn();
-    if (googleUser == null ) return;
-    
-    _user = googleUser;
-    // Getting the tokens to make a credential
-    final googleAuth = await googleUser.authentication;
-    // Making a credential to sign in to firebase
-    final credential = GoogleAuthProvider.credential(
-      accessToken: googleAuth.accessToken,
-      idToken: googleAuth.idToken,
-    );
-    // Uploading that credential to firebase
-    final UserCredential? result = await FirebaseAuth.instance.signInWithCredential(credential);
-    
     User? userDetails = result?.user;
 
-     if (result != null) {
-       // Saving user data locally
-       SharedPreferencesHelper().saveUserId(userDetails!.uid);
-       SharedPreferencesHelper().saveUserEmail(userDetails.email);
-       SharedPreferencesHelper().saveDisplayName(userDetails.displayName);
-       SharedPreferencesHelper().saveUserProfile(userDetails.photoURL);
-       // Mapping data to add user to firebase
-       Map<String, dynamic> userInfoMap = {
-         "email": userDetails.email,
-         "username": userDetails.email!.replaceAll("@gmail.com", ""),
-         "name": userDetails.displayName,
-         "imgUrl": userDetails.photoURL,
-       };
-       DatabaseMethods().addUserInfoTodb(userDetails.uid, userInfoMap).then(
-           (value) => Navigator.pushReplacement(context,
-               MaterialPageRoute(builder: (context) => const HomeScreen())));
-     }
-    notifyListeners();  
-  }
+    if (result != null) {
+      // Saving user data locally
+      SharedPreferencesHelper().saveUserId(userDetails!.uid);
+      SharedPreferencesHelper().saveUserEmail(userDetails.email);
+      SharedPreferencesHelper().saveDisplayName(userDetails.displayName);
+      SharedPreferencesHelper().saveUserProfile(userDetails.photoURL);
+      // Mapping data to add user to firebase
+      Map<String, dynamic> userInfoMap = {
+        "email": userDetails.email,
+        "username": userDetails.email!.replaceAll("@gmail.com", ""),
+        "name": userDetails.displayName,
+        "imgUrl": userDetails.photoURL,
+      };
 
-  
+      DatabaseMethods().addUserInfoTodb(userDetails.uid, userInfoMap).then(
+          (value) => Navigator.pushReplacement(context,
+              MaterialPageRoute(builder: (context) => const HomeScreen())));
+    }
+  }
 }
