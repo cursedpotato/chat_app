8653bcdef15d620a9faaa63c0133ef011b253eee
Daniel <danireysan@gmail.com>
2022-03-21 11:14:19 -0600

    implementing a dark theme and adding text to the signin page

--- lib/main.dart
+++ lib/main.dart
@@ -19,8 +19,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      theme: ThemeData.dark(
+        
       ),
       home: FutureBuilder(
         future: AuthMethods().getCurrentUser(),
--- lib/views/signin.dart
+++ lib/views/signin.dart
@@ -18,21 +18,26 @@ class _SignInState extends State<SignIn> {
         title: const Text("Messenger Clone"),
       ),
       body: Center(
-        child: GestureDetector(
-          onTap: () {
-            AuthMethods().signInWithGoogle(context);
-          },
-          child: Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(24),
-              color: const Color(0xffDB4437),
+        child: Column(
+          children: [
+            Text("Welcome to Capychat", style: TextStyle(fontSize: 20),),
+            GestureDetector(
+              onTap: () {
+                AuthMethods().signInWithGoogle(context);
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(24),
+                  color: const Color(0xffDB4437),
+                ),
+                padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                child: const Text(
+                  "Sign In with Google",
+                  style: TextStyle(fontSize: 16, color: Colors.white),
+                ),
+              ),
             ),
-            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-            child: const Text(
-              "Sign In with Google",
-              style: TextStyle(fontSize: 16, color: Colors.white),
-            ),
-          ),
+          ],
         ),
       ),
     );
