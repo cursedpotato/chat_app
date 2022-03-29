56594941d7062cb600186379b3ce16676f0b9973
Daniel <danireysan@gmail.com>
2022-03-29 10:50:17 -0600

    added notification service

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:chat_app/services/auth.dart';
+import 'package:chat_app/services/notifications_services.dart';
 import 'package:chat_app/views/home.dart';
 import 'package:chat_app/views/signin.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await NotificationService().init();
   await Firebase.initializeApp();
   runApp(const MyApp());
 }
--- lib/services/notifications_services.dart
+++ lib/services/notifications_services.dart
@@ -1,14 +1,60 @@
+import 'package:chat_app/views/chatscreen.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class NotificationService {
+  //Singleton pattern
   static final NotificationService _notificationService =
       NotificationService._internal();
-
   factory NotificationService() {
     return _notificationService;
   }
-
   NotificationService._internal();
 
-  
+  //instance of FlutterLocalNotificationsPlugin
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  Future<void> init() async {
+    //Initialization Settings for Android
+    const AndroidInitializationSettings initializationSettingsAndroid =
+        AndroidInitializationSettings('app_icon');
+
+    //Initialization Settings for iOS
+    const IOSInitializationSettings initializationSettingsIOS =
+        IOSInitializationSettings(
+      requestSoundPermission: false,
+      requestBadgePermission: false,
+      requestAlertPermission: false,
+    );
+
+    //InitializationSettings for initializing settings for both platforms (Android & iOS)
+    const InitializationSettings initializationSettings =
+        InitializationSettings(
+            android: initializationSettingsAndroid,
+            iOS: initializationSettingsIOS);
+
+    await flutterLocalNotificationsPlugin.initialize(
+      initializationSettings,
+    );
+  }
+
+  void requestIOSPermissions(
+    FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin) {
+  flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<
+          IOSFlutterLocalNotificationsPlugin>()
+      ?.requestPermissions(
+        alert: true,
+        badge: true,
+        sound: true,
+      );
+}
+
+  Future selectNotification(BuildContext context, String chatWithUsername, name   ) async {
+    await Navigator.push(
+      context,
+      MaterialPageRoute<void>(builder: (context) => ChatScreen(chatWithUsername, name)),
+    );
+}
 }
