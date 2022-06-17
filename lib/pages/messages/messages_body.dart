36d5a8f02208a681e5f5673dee2f78a6bc95df6d
Daniel <danireysan@gmail.com>
2022-06-16 21:40:34 -0500

    created audio message file

--- lib/pages/messages/messages_body.dart
+++ lib/pages/messages/messages_body.dart
@@ -3,6 +3,8 @@ import 'package:chat_app/pages/messages/chat_input_field.dart';
 import 'package:chat_app/pages/messages/text_message.dart';
 import 'package:flutter/material.dart';
 
+import 'audio_message.dart';
+
 enum ChatMessageType { text, audio, image, video }
 enum MessageStatus { not_sent, not_view, viewed }
 
@@ -132,60 +134,4 @@ class Message extends StatelessWidget {
   }
 }
 
-class AudioMessage extends StatelessWidget {
-  final ChatMessage message;
-  const AudioMessage({Key? key, required this.message}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width * 0.55,
-      padding: const EdgeInsets.symmetric(
-        horizontal: kDefaultPadding * 0.75,
-        vertical: kDefaultPadding / 2.5,
-      ),
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(30),
-          color: kPrimaryColor.withOpacity(message.isSender ? 1 : 0.1)),
-      child: Row(
-        children: [
-          Icon(Icons.play_arrow,
-              color: message.isSender ? Colors.white : kPrimaryColor),
-          Expanded(
-            child: Stack(
-              clipBehavior: Clip.none,
-              alignment: Alignment.center,
-              children: [
-                Container(
-                  width: double.infinity,
-                  height: 2,
-                  color: message.isSender
-                      ? Colors.white
-                      : kPrimaryColor.withOpacity(0.4),
-                ),
-                Positioned(
-                  left: 0,
-                  child: Container(
-                    height: 8,
-                    width: 8,
-                    decoration: BoxDecoration(
-                      color: message.isSender? Colors.white : kPrimaryColor,
-                      shape: BoxShape.circle,
-                    ),
-                  ),
-                )
-              ],
-            ),
-          ),
-          Text(
-            "0:37",
-            style: TextStyle(
-              fontSize: 12,
-              color: message.isSender ? Colors.white : null,
-            ),
-          )
-        ],
-      ),
-    );
-  }
-}
