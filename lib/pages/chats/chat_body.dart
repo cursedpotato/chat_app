1970848301a08a1d7152d28ff15b45198b396cdd
Daniel <danireysan@gmail.com>
2022-07-21 19:06:10 -0500

    trying to implement home stream

--- lib/pages/chats/chat_body.dart
+++ lib/pages/chats/chat_body.dart
@@ -1,9 +1,56 @@
 import 'package:chat_app/globals.dart';
+import 'package:chat_app/services/database.dart';
 import 'package:chat_app/widgets/filledout_button.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:random_string/random_string.dart';
 
-class Body extends StatelessWidget {
-  const Body({Key? key}) : super(key: key);
+import '../../helperfunctions/sharedpref_helper.dart';
+
+class Body extends StatefulWidget {
+  @override
+  State<Body> createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  bool isSearching = false;
+  String? myName, myProfilePic, myUserName, myEmail;
+  Stream? usersStream, chatRoomsStream;
+
+  TextEditingController searchUsernameEditingController =
+      TextEditingController();
+
+  getMyInfoFromSharedPreference() async {
+    myName = await SharedPreferenceHelper().getDisplayName();
+    myProfilePic = await SharedPreferenceHelper().getUserProfileUrl();
+    myUserName = await SharedPreferenceHelper().getUserName();
+    myEmail = await SharedPreferenceHelper().getUserEmail();
+    setState(() {});
+  }
+
+  getChatRoomIdByUsernames(String a, String b) {
+    if (a.substring(0, 1).codeUnitAt(0) > b.substring(0, 1).codeUnitAt(0)) {
+      return "$b\_$a";
+    } else {
+      return "$a\_$b";
+    }
+  }
+
+  getChatRooms() async {
+    chatRoomsStream = await DatabaseMethods().getChatRooms();
+    setState(() {});
+  }
+
+  onScreenLoaded() async {
+    await getMyInfoFromSharedPreference();
+    getChatRooms();
+  }
+
+  @override
+  void initState() {
+    onScreenLoaded();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,7 +82,8 @@ class Body extends StatelessWidget {
         Expanded(
           child: ListView.builder(
               itemCount: 10,
-              itemBuilder: (BuildContext context, int index) => const ChatCard()),
+              itemBuilder: (BuildContext context, int index) =>
+                  ChatCard()),
         )
       ],
     );
@@ -43,71 +91,79 @@ class Body extends StatelessWidget {
 }
 
 class ChatCard extends StatelessWidget {
-  const ChatCard({
+  Stream<QuerySnapshot> homeStream;
+  ChatCard({ 
+    required 
     Key? key,
+    required homeStream
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(
-          horizontal: kDefaultPadding, vertical: kDefaultPadding * 0.75),
-      child: Row(
-        children: [
-          Stack(
-            children: [
-              const CircleAvatar(
-                radius: 24,
-                backgroundImage: NetworkImage(
-                    "https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png"),
-              ),
-              // TODO: add conditional to check if user is active
-              Positioned(
-                right: 0,
-                bottom: 0,
-                child: Container(
-                  height: 16,
-                  width: 16,
-                  decoration: BoxDecoration(
-                    color: kPrimaryColor,
-                    shape: BoxShape.circle,
-                    border: Border.all(
-                      color: Theme.of(context).scaffoldBackgroundColor,
-                    )
-                  ),
+    return StreamBuilder(
+      stream: stream,
+      
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        return Padding(
+        padding: const EdgeInsets.symmetric(
+            horizontal: kDefaultPadding, vertical: kDefaultPadding * 0.75),
+        child: Row(
+          children: [
+            Stack(
+              children: [
+                const CircleAvatar(
+                  radius: 24,
+                  backgroundImage: NetworkImage(
+                      "https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png"),
                 ),
-              )
-            ],
-          ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: const [
-                  Text(
-                    "name",
-                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.w500),
+                // TODO: add conditional to check if user is active
+                Positioned(
+                  right: 0,
+                  bottom: 0,
+                  child: Container(
+                    height: 16,
+                    width: 16,
+                    decoration: BoxDecoration(
+                        color: kPrimaryColor,
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                          color: Theme.of(context).scaffoldBackgroundColor,
+                        )),
                   ),
-                  SizedBox(height: 8),
-                  Opacity(
-                    opacity: 0.64,
-                    child: Text(
-                      "lastmessage",
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
+                )
+              ],
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text(
+                      "name",
+                      style: TextStyle(fontSize: 15, fontWeight: FontWeight.w500),
                     ),
-                  )
-                ],
+                    SizedBox(height: 8),
+                    Opacity(
+                      opacity: 0.64,
+                      child: Text(
+                        "lastmessage",
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
-          ),
-          const Opacity(
-            opacity: 0.64,
-            child: Text("time"),
-          )
-        ],
+            const Opacity(
+              opacity: 0.64,
+              child: Text("time"),
+            )
+          ],
+        ),
       ),
-    );
+      },
+    ),
   }
 }
