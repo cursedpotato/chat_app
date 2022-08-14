9b533207a12758ea00d7af6b7b36928a80e3716f
Daniel <danireysan@gmail.com>
2022-08-13 21:23:39 -0500

    added conditional in chatroom stream builder

--- lib/screens/home/chat_body.dart
+++ lib/screens/home/chat_body.dart
@@ -19,9 +19,7 @@ class Body extends HookWidget {
 
     // This variable was created to filter chatroom stream data and toggle buttons
 
-    ValueNotifier<bool> isActive = useValueNotifier(false);
-
-    // TODO: implement use state for is active var
+    ValueNotifier isActive = useState(false);
     return Column(
       children: [
         Container(
@@ -32,58 +30,63 @@ class Body extends HookWidget {
             kDefaultPadding,
           ),
           color: kPrimaryColor,
-          child: HookBuilder(
-            builder: (_) {
-              bool toggle = useValueListenable(isActive);
-              return Row(
-                children: [
-                  FillOutlineButton(
-                    press: () => isActive.value = !isActive.value,
-                    text: "Recent Messages",
-                    isFilled: !toggle,
-                  ),
-                  const SizedBox(width: kDefaultPadding),
-                  FillOutlineButton(
-                    press: () => isActive.value = !isActive.value,
-                    text: "Active",
-                    isFilled: toggle,
-                  ),
-                ],
-              );
-            },
+          child: Row(
+            children: [
+              FillOutlineButton(
+                press: () => isActive.value = !isActive.value,
+                text: "Recent Messages",
+                isFilled: !isActive.value,
+              ),
+              const SizedBox(width: kDefaultPadding),
+              FillOutlineButton(
+                press: () => isActive.value = !isActive.value,
+                text: "Active",
+                isFilled: isActive.value,
+              ),
+            ],
           ),
         ),
         StreamBuilder(
           stream: chatroomStream,
           builder:
               (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-            bool isWaiting =
-                snapshot.connectionState == ConnectionState.waiting;
+            bool isWaiting = snapshot.connectionState == ConnectionState.waiting;
             if (isWaiting) {
               return const LinearProgressIndicator();
             }
-            bool hasData = snapshot.hasData;
-            if (hasData) {
-              // TODO: Add conditional that filters if users are active or nots
-              List<DocumentSnapshot> documentList = snapshot.data!.docs;
-              return Expanded(
-                child: ListView.builder(
-                  itemCount: documentList.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    DocumentSnapshot documentSnapshot = documentList[index];
 
-                    return ChatCard(
-                      documentSnapshot: documentSnapshot,
-                    );
-                  },
-                ),
-              );
+            bool isRecent = snapshot.hasData && !isActive.value;
+            if (isRecent) {
+              // TODO: Add conditional that filters if users are active or not
+              List<DocumentSnapshot> documentList = snapshot.data!.docs;
+              return chatroomLb(documentList);
             }
+
+            bool isAvailable = snapshot.hasData && isActive.value;
+            if (isAvailable) {
+              List<DocumentSnapshot> documentList = snapshot.data!.docs;
+              return chatroomLb(documentList);
+            }
+
             // TODO: Make an error screen
             return const Text("Something went wrong");
           },
         ),
       ],
+    );
+  }
+
+  Widget chatroomLb(List<DocumentSnapshot> documentList) {
+    return Expanded(
+      child: ListView.builder(
+        itemCount: documentList.length,
+        itemBuilder: (BuildContext context, int index) {
+          DocumentSnapshot documentSnapshot = documentList[index];
+          return ChatCard(
+            documentSnapshot: documentSnapshot,
+          );
+        },
+      ),
     );
   }
 }
