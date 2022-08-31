40f00cc3bde507d9096655a3418af0f740cfbacc
Daniel <danireysan@gmail.com>
2022-08-30 19:25:25 -0500

    Assigned global variable for database method

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/globals.dart';
 import 'package:flutter/material.dart';
 
 class ProfileScreen extends StatelessWidget {
@@ -5,6 +6,30 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text("Profile"),);
+    return SafeArea(
+      child: Column(
+        children: [
+          Center(
+            child: Stack(
+              children: [
+                const CircleAvatar(
+                  radius: 50,
+                  backgroundImage: NetworkImage(
+                      "https://icon-library.com/images/no-user-image-icon/no-user-image-icon-3.jpg"),
+                ),
+                Positioned(
+                    top: 60,
+                    left: 60,
+                    child: IconButton(
+                        onPressed: () {}, icon: const Icon(Icons.camera_alt_outlined))),
+              ],
+            ),
+          ),
+          Text(chatterUsername!),
+        ],
+
+        
+      ),
+    );
   }
 }
