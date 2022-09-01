30e8a4057c6954dd39856ad1d97ed9f54ce9f29f
Daniel <danireysan@gmail.com>
2022-08-31 21:28:29 -0500

    removed rtc engine

--- lib/screens/calls_screen.dart
+++ lib/screens/calls_screen.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
-class CallScreen extends StatelessWidget {
+class CallScreen extends StatefulWidget {
   const CallScreen({Key? key}) : super(key: key);
 
+  @override
+  State<CallScreen> createState() => _CallScreenState();
+}
+
+class _CallScreenState extends State<CallScreen> {
+  
   @override
   Widget build(BuildContext context) {
     return const Center(child: Text("Calls Screen"),);
