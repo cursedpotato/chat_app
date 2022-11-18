3aa65cc1a33580c7ccf319f94ee7a6418b898647
Daniel <danireysan@gmail.com>
2022-11-17 18:45:49 -0600

    fixed player counter

--- lib/screens/chatroom/message_types/audio_message_widget.dart
+++ lib/screens/chatroom/message_types/audio_message_widget.dart
@@ -15,10 +15,6 @@ class AudioMessage extends HookWidget {
     final player = AudioPlayer();
 
     preparePlayer() async {
-      player.playbackEventStream.listen((event) {},
-          onError: (Object e, StackTrace stackTrace) {
-        print('A stream error occurred: $e');
-      });
       try {
         await player.setUrl(message.resUrl!);
       } catch (e) {
@@ -96,22 +92,37 @@ class PlayButton extends HookWidget {
   }
 }
 
+
+
 class Counter extends HookWidget {
   const Counter({Key? key, required this.audioPlayer}) : super(key: key);
   final AudioPlayer audioPlayer;
   @override
   Widget build(BuildContext context) {
-    String twoDigits(int n) => n.toString().padLeft(2, '0');
+    final bool isPlaying = useStream(audioPlayer.playerStateStream).data?.playing ?? false;
 
-    final counterSnapshot = useStream(audioPlayer.durationStream);
+    final durationSnapshot = useStream(audioPlayer.durationStream);
 
-    String seconds =
-        twoDigits(counterSnapshot.requireData?.inSeconds.remainder(60) ?? 0);
-    String minutes =
-        counterSnapshot.data?.inMinutes.remainder(60).toString() ?? '0';
+    final positionSnapshot = useStream(audioPlayer.positionStream);
 
-    ValueNotifier<String> time = useState('$minutes:$seconds');
 
-    return Text(time.value);
+    if (!durationSnapshot.hasData) return const SizedBox();
+
+    timeFormat(Duration duration) {
+      String twoDigits(int n) => n.toString().padLeft(2, '0');
+      String seconds = twoDigits(duration.inSeconds.remainder(60));
+      String minutes = duration.inMinutes.toString();
+      final map = {
+        'seconds' : seconds,
+        'minutes' : minutes
+      };
+      return map;
+    }
+
+    ValueNotifier<Map> time = useState(timeFormat(durationSnapshot.data!));
+
+    if (isPlaying) time.value = timeFormat(positionSnapshot.data!) ;
+
+    return Text('${time.value["minutes"]}:${time.value["seconds"]}');
   }
 }
