58aa8a084cb7550ccada056a711e95be06a585a9
Daniel <danireysan@gmail.com>
2022-12-10 20:08:31 -0600

    Created new storage method

--- lib/screens/chatroom/message_types/media_message.dart
+++ lib/screens/chatroom/message_types/media_message.dart
@@ -1,10 +1,15 @@
+import 'dart:io';
+
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:chat_app/models/message_model.dart';
+import 'package:dio/dio.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:photo_view/photo_view.dart';
 import 'package:photo_view/photo_view_gallery.dart';
+import 'package:video_compress/video_compress.dart';
 
 class MediaType {
   final String mediaUrl;
@@ -24,8 +29,8 @@ class MediaMessageWidget extends HookWidget {
     final urlList =
         useStream(useMemoized(() => contentType(chatMessagelModel.resUrls)));
 
-    if (urlList.hasData) {
-      print("TAG: ${urlList.data}");
+    if (!urlList.hasData) {
+      return const CircularProgressIndicator();
     }
 
     return GestureDetector(
@@ -49,7 +54,9 @@ class MediaMessageWidget extends HookWidget {
     }
 
     if (listLenght == 1 && contentList[0].isVideo) {
-      return const CircularProgressIndicator();
+      return SingleVideoWidget(
+        mediaType: contentList[0],
+      );
     }
 
     return SingleImageWidget(mediaUrl: contentList[0].mediaUrl);
@@ -92,14 +99,53 @@ class SingleImageWidget extends StatelessWidget {
   }
 }
 
-class SingleVideoWidget extends StatelessWidget {
-  final ChatMesssageModel chatMesssageModel;
-  const SingleVideoWidget({Key? key, required this.chatMesssageModel})
+class SingleVideoWidget extends HookWidget {
+  final MediaType mediaType;
+  const SingleVideoWidget({Key? key, required this.mediaType})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    downloadVideo() async {
+      final path = await getTemporaryDirectory();
+      final fullPath = '${path.path}/videos}';
+      bool hasFile = (await File(fullPath).exists());
+
+      if (!hasFile) {
+        debugPrint("Downloading audio file");
+        final response = await Dio().download(mediaType.mediaUrl, fullPath);
+        if (response.statusCode == 200) {
+          return File(fullPath);
+        }
+      }
+      return File(fullPath);
+    }
+
+    getThumbnail(File videoFile) async {
+      final thumbnailFile = await VideoCompress.getFileThumbnail(
+        videoFile.path,
+        quality: 50, // default(100)
+        position: -1, // default(-1)
+      );
+      return thumbnailFile;
+    }
+
+    final videoFile = useFuture(useMemoized(
+      () => downloadVideo(),
+    ));
+
+    if (!videoFile.hasData) {
+      return const CircularProgressIndicator();
+    }
+
+    final videoThumbnail =
+        useFuture(useMemoized(() => getThumbnail(videoFile.data!)));
+
+    if (!videoThumbnail.hasData) {
+      return const CircularProgressIndicator();
+    }
+
+    return Image(image: FileImage(videoThumbnail.requireData));
   }
 }
 
--- lib/services/storage_methods.dart
+++ lib/services/storage_methods.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:video_compress/video_compress.dart';
 
 class StorageMethods {
   final FirebaseStorage _storage = FirebaseStorage.instance;
@@ -15,14 +16,30 @@ class StorageMethods {
 
     TaskSnapshot snapshot = await uploadFile;
     String downloadUrl = await snapshot.ref.getDownloadURL().then((value) {
-
-      
       reference.updateMetadata(SettableMetadata(contentType: "image"));
-      
+
       if (isVideo) {
         reference.updateMetadata(SettableMetadata(contentType: "video/mp4"));
       }
 
+      return value;
+    });
+
+    return downloadUrl;
+  }
+
+  Future<String> uploadThumbnail(File videoFile, String id) async {
+    final thumbnailFile = await VideoCompress.getFileThumbnail(
+      videoFile.path,
+      quality: 50, // default(100)
+      position: -1, // default(-1)
+    );
+    Reference reference = _storage.ref().child('thumbnails').child(id);
+    UploadTask uploadFile = reference.putFile(thumbnailFile);
+    TaskSnapshot snapshot = await uploadFile;
+
+    String downloadUrl = await snapshot.ref.getDownloadURL().then((value) {
+      reference.updateMetadata(SettableMetadata(contentType: "image"));
       return value;
     });
 
