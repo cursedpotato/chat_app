acf135bc7572afbb495b402e9bb3783beb11d859
Daniel <danireysan@gmail.com>
2022-12-30 23:59:47 -0600

    creating  a camera provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:camera/camera.dart';
 import 'package:chat_app/screens/home/home_screen.dart';
 
 import 'package:chat_app/theme.dart';
@@ -5,25 +6,40 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'firebase_options.dart';
 
 import 'screens/signin/signin_screen.dart';
 
+final camerasList = StateProvider(
+  (ref) => [],
+);
+
+late List<CameraDescription> _cameras;
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  _cameras = await availableCameras();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(
     const ProviderScope(child: MyApp()),
   );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends HookConsumerWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    
+
+    useEffect(() {
+      
+      ref.read(camerasList.notifier).state = _cameras;
+      return ;
+    }, []);
     getCurrentUser() async {
       return FirebaseAuth.instance.currentUser;
     }
